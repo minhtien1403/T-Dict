c095ac0fbbdc7fd3a17a0239d47a22f911867b35
minhtien1403 <minhtiencp1403@gmail.com>
2021-04-12 20:06:35 +0700

    remind user about about their searched word

--- T-Dict/T-Dict/Controller/RemindVC/RemindViewController.swift
+++ T-Dict/T-Dict/Controller/RemindVC/RemindViewController.swift
@@ -7,15 +7,52 @@
 //
 
 import UIKit
+import Then
 
 final class RemindViewController: UIViewController {
+    
+    @IBOutlet private weak var stepLabel: UILabel!
+    @IBOutlet private weak var stepper: UIStepper!
+    @IBOutlet private weak var notiSwitch: UISwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        stepLabel.text = "\(AppSetting.stepperCurrentValue)"
+        stepper.value = Double(AppSetting.stepperCurrentValue)
+        notiSwitch.setOn(AppSetting.switchCurrentState, animated: true)
+    }
+    
+    
     func configView() {
         title = "Reminder"
+        stepper.do {
+            $0.minimumValue = 0
+            $0.maximumValue = 5
+        }
+        NotificationServices.shared.deniedPermission = { [weak self] in
+            DispatchQueue.main.async {
+                self?.alertError(message: "This function will not working correct if you denied notification permission")
+            }
+        }
+        NotificationServices.shared.authorizeNotification()
+    }
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        AppSetting.stepperCurrentValue = Int(sender.value)
+        stepLabel.text = "\(AppSetting.stepperCurrentValue)"
+    }
+    
+    @IBAction func switchDidVChange(_ sender: UISwitch) {
+        if sender.isOn {
+            NotificationServices.shared.setNofication(times: AppSetting.stepperCurrentValue)
+        } else {
+            NotificationServices.shared.removePendingNotification()
+        }
+        AppSetting.switchCurrentState = sender.isOn
     }
 }
